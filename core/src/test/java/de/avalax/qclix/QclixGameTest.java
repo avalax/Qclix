1be8afc8e5c0c7c5d18a8110e7d4a436b84547bf
Benjamin <github@avalax.de>
2017-02-04 10:45:15 +0100

    added jUnit

---
+++ core/src/test/java/de/avalax/qclix/QclixGameTest.java
@@ -0,0 +1,11 @@
+package de.avalax.qclix;
+
+import org.junit.Test;
+
+public class QclixGameTest {
+
+    @Test
+    public void shouldInitiate() throws Exception {
+        new QclixGame();
+    }
+}
